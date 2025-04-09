b553b5b3cf5fcb8b29337cdd0d35d18417d1a48a
Anime <franciscotadeuuu@gmail.com>
2025-04-09 20:07:26 -0300

    part 2

--- app/(tabs)/criar.tsx
+++ app/(tabs)/criar.tsx
@@ -11,107 +11,162 @@ import {
 } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { Link, router } from "expo-router";
-import { Ionicons, Entypo, FontAwesome } from "@expo/vector-icons";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import Feather from "@expo/vector-icons/Feather";
 import Zocial from "@expo/vector-icons/Zocial";
+import { useForm, Controller } from "react-hook-form";
+
 const image1 = {
   uri: "https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/087933cd6602ba9437e981dacc320608",
 };
 
 export default function Criar() {
-  const [olho, setOlho] = useState("");
   const [ver, setVer] = useState(true);
-  const [erro, setErro] = useState(null);
-  const [usuario, setUsuario] = useState("");
-  const [senha, setSenha] = useState("");
-  const [email, setEmail] = useState("");
-  const [nome, setNome] = useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function verificador() {
-    const sav = {
-      usuario,
-      email,
-      nome,
-      senha,
-    };
-    console.log(sav);
-  }
+  const onSubmit = (data: any) => {
+    console.log(data);
+    router.push("/inicio");
+  };
 
   return (
     <ImageBackground source={image1} style={x.imagefundo}>
       <KeyboardAvoidingView behavior="height" enabled>
         <View style={x.criar}>
-          <View>
-            <View>
-              <View>
-                <Text style={x.ali}>Criar Conta</Text>
-              </View>
-              <View>
-                <Pressable onPress={() => router.back()}>
-                  <Ionicons
-                    name="arrow-back"
-                    size={24}
-                    color="black"
-                    style={x.al}
-                  />
-                </Pressable>
-              </View>
-
-              <TextInput
-                placeholder="Usuário"
-                style={x.text}
-                value={usuario}
-                onChangeText={setUsuario}
-              />
-              <FontAwesome
-                name="user-o"
+          <Text style={x.ali}>Criar Conta</Text>
+          <TouchableOpacity>
+            <Pressable onPress={() => router.back()}>
+              <Ionicons
+                name="arrow-back"
                 size={32}
                 color="black"
-                style={x.user}
+                style={x.al}
               />
-            </View>
-            <TextInput
-              placeholder="Nome Completo"
-              style={x.text}
-              value={nome}
-              onChangeText={setNome}
-            />
-            <Feather name="user" size={32} color="black" style={x.user} />
-          </View>
-          <View>
-            <TextInput
-              placeholder="Email"
-              style={x.text}
-              value={email}
-              onChangeText={setEmail}
-            />
-            <Zocial name="gmail" size={32} color="black" style={x.user} />
-          </View>
-          <View>
-            <TextInput
-              placeholder="Senha"
-              style={x.text}
-              value={senha}
-              onChangeText={setSenha}
-              secureTextEntry={ver}
-            />
-          </View>
-          <TouchableOpacity onPress={() => setVer(!ver)}>
-            {ver ? (
-              <FontAwesome
-                name="eye-slash"
-                size={32}
-                color="black"
-                style={x.olho}
-              />
-            ) : (
-              <FontAwesome name="eye" size={32} color="black" style={x.olho} />
-            )}
+            </Pressable>
           </TouchableOpacity>
-          <TouchableOpacity style={x.bota} onPress={verificador}>
-            <Link href="/inicio" style={x.botatext}>
-              ACESSAR
-            </Link>
+
+          <Controller
+            control={control}
+            rules={{ required: "Campo obrigatório" }}
+            render={({ field: { onChange, value } }) => (
+              <View>
+                <TextInput
+                  placeholder="Usuário"
+                  style={x.text}
+                  value={value}
+                  onChangeText={onChange}
+                />
+                <FontAwesome
+                  name="user-o"
+                  size={32}
+                  color="black"
+                  style={x.user}
+                />
+                {errors.usuario && (
+                  <Text style={x.errorText}>
+                    {errors.usuario.message?.toString()}
+                  </Text>
+                )}
+              </View>
+            )}
+            name="usuario"
+          />
+
+          <Controller
+            control={control}
+            rules={{ required: "Campo obrigatório" }}
+            render={({ field: { onChange, value } }) => (
+              <View>
+                <TextInput
+                  placeholder="Nome Completo"
+                  style={x.text}
+                  value={value}
+                  onChangeText={onChange}
+                />
+                <Feather name="user" size={32} color="black" style={x.user} />
+                {errors.nome && (
+                  <Text style={x.errorText}>
+                    {errors.nome.message?.toString()}
+                  </Text>
+                )}
+              </View>
+            )}
+            name="nome"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: " Email obrigatório",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Email inválido",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <View>
+                <TextInput
+                  placeholder="Email"
+                  style={x.text}
+                  value={value}
+                  onChangeText={onChange}
+                />
+                <Zocial name="gmail" size={32} color="black" style={x.user} />
+                {errors.email && (
+                  <Text style={x.errorText}>
+                    {errors.email.message?.toString()}
+                  </Text>
+                )}
+              </View>
+            )}
+            name="email"
+          />
+
+          <Controller
+            control={control}
+            rules={{ required: "Campo Senha é obrigatório" }}
+            render={({ field: { onChange, value } }) => (
+              <View>
+                <TextInput
+                  placeholder="Senha"
+                  style={x.text}
+                  value={value}
+                  onChangeText={onChange}
+                  secureTextEntry={ver}
+                />
+                <TouchableOpacity onPress={() => setVer(!ver)}>
+                  {ver ? (
+                    <FontAwesome
+                      name="eye-slash"
+                      size={32}
+                      color="black"
+                      style={x.olho}
+                    />
+                  ) : (
+                    <FontAwesome
+                      name="eye"
+                      size={32}
+                      color="black"
+                      style={x.olho}
+                    />
+                  )}
+                </TouchableOpacity>
+                {errors.senha && (
+                  <Text style={x.errorText}>
+                    {errors.senha.message?.toString()}
+                  </Text>
+                )}
+              </View>
+            )}
+            name="senha"
+          />
+
+          <TouchableOpacity style={x.bota} onPress={handleSubmit(onSubmit)}>
+            <Text style={x.botatext}>ACESSAR</Text>
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
@@ -139,14 +194,13 @@ const x = StyleSheet.create({
   },
   text: {
     fontSize: 24,
-    marginTop: 36,
+    marginTop: 124,
     marginLeft: 10,
     borderWidth: 5,
     borderRadius: 20,
-    marginBottom: -26,
+    marginBottom: -96,
     width: "100%",
   },
-
   ali: {
     backgroundColor: "green",
     marginTop: -198,
@@ -158,7 +212,7 @@ const x = StyleSheet.create({
     paddingLeft: 250,
   },
   al: {
-    marginTop: -188,
+    marginTop: -68,
     marginLeft: "-85%",
     marginRight: "8%",
     fontSize: 55,
@@ -174,7 +228,7 @@ const x = StyleSheet.create({
     width: "70%",
     left: "1%",
     borderRadius: 120,
-    marginTop: 50,
+    marginTop: 130,
     alignItems: "center",
     justifyContent: "center",
     marginLeft: 50,
@@ -191,7 +245,14 @@ const x = StyleSheet.create({
     left: "89%",
   },
   user: {
-    marginTop: -20,
+    marginTop: -10,
     left: "88%",
+  },
+  errorText: {
+    color: "#ff375b",
+    fontSize: 17,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginTop: 8,
   },
 });

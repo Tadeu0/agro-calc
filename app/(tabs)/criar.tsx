d9a13e9cc2e9d022cce14fc69af7e2720bc19913
Anime <franciscotadeuuu@gmail.com>
2025-04-11 10:47:59 -0300

    1

--- app/(tabs)/criar.tsx
+++ app/(tabs)/criar.tsx
@@ -37,7 +37,7 @@ export default function Criar() {
 
   const onSubmit = (data: any) => {
     console.log("Dados:", data);
-    router.push("/inicio");
+    router.push("/(tabs)/index");
   };
 
   return (
@@ -52,7 +52,6 @@ export default function Criar() {
           </Pressable>
           <Text style={styles.titulo}>Criar Conta</Text>
 
-          {/* Usuário */}
           <Controller
             control={control}
             name="usuario"

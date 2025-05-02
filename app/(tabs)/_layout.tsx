b5b2602e9e53aa3e6ea053130457a51accb002da
Anime <franciscotadeuuu@gmail.com>
2025-05-02 15:18:29 -0300

    3

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,6 @@ import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "../../constants/Colors";
 
-
 export default function TabLayout() {
   return (
     <Tabs
@@ -15,15 +14,30 @@ export default function TabLayout() {
     >
       <Tabs.Screen
         name="index"
-        options={{ title: "Início", tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} /> }}
+        options={{
+          title: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="Principal"
-        options={{ title: "Histórico", tabBarIcon: ({ color, size }) => <Ionicons name="list" color={color} size={size} /> }}
+        options={{
+          title: "Histórico",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="Calculos"
-        options={{ title: "Cálculos", tabBarIcon: ({ color, size }) => <Ionicons name="calculator" color={color} size={size} /> }}
+        options={{
+          title: "Cálculos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calculator" color={color} size={size} />
+          ),
+        }}
       />
     </Tabs>
   );
